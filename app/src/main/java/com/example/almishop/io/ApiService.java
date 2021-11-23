6a72954e5ccce6fb8a70d85075002975c95662dd
ianbroca <ianbroca@gmail.com>
2021-11-23 12:53:00 +0100

    Añadida llamada de cambio de contraseña.

--- app/src/main/java/com/example/almishop/io/ApiService.java
+++ app/src/main/java/com/example/almishop/io/ApiService.java
@@ -2,6 +2,7 @@ package com.example.almishop.io;
 
 import androidx.annotation.AnyRes;
 
+import com.example.almishop.model.ChangePassword;
 import com.example.almishop.model.Location;
 import com.example.almishop.model.Login;
 import com.example.almishop.model.Register;
@@ -11,6 +12,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiService {
@@ -33,4 +35,7 @@ public interface ApiService {
 
     @POST("user/location")
     Call<AnyRes> sendLocation(@Body Location data);
+
+    @PUT("user/edit")
+    Call<User> changePassword(@Body ChangePassword data);
 }
